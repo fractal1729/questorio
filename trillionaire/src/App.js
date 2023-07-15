374ae6799518527ccc30b8663a02c8f809a9f7e8
Vinjai Vale <vinjai@watershedclimate.com>
2023-07-15 14:36:37 -0700

    deets

--- trillionaire/src/App.js
+++ trillionaire/src/App.js
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import { Box, Container, CssBaseline } from '@mui/material';
 import Chatbox from './components/Chatbox';
 import Input from './components/Input';
+import { Configuration, OpenAIApi } from "openai";
+
+let configuration = new Configuration({
+  apiKey: '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',//process.env.OPENAI_API_KEY_SCALE_HACKDAY,
+});
+delete configuration.baseOptions.headers['User-Agent'];
+const openai = new OpenAIApi(configuration);
+console.log(openai);
 
 function App() {
   const [chat, setChat] = useState([]);
 
-  const handleSend = (message) => {
+  const handleSend = async (message) => {
     setChat([...chat, { message, sender: 'user' }]);
-    setTimeout(() => {
-      setChat((prevChat) => [...prevChat, { message: 'I am a bot', sender: 'bot' }]);
-    }, 1000);
+    try {
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {"role": "system", "content": "You are a helpful assistant."},
+          {"role": "user", "content": message},
+        ],
+        temperature: 0.8,
+      });
+      const { choices } = response.data;
+      setChat((prevChat) => [...prevChat, { message: choices[0].message.content, sender: 'bot' }]);
+    }
+    catch (e) {
+      console.error(e);
+    }
   };
 
   return (
--- trillionaire/src/components/Input.js
+++ trillionaire/src/components/Input.js
@@ -3,8 +3,9 @@ import { Box, TextField, Button } from '@mui/material';
 
 const Input = ({ onSend }) => {
   const [message, setMessage] = useState('');
+  const [fetching, setFetching] = useState(false);
 
-  const handleSend = (event) => {
+  const handleSend = async (event) => {
     // Prevent the form from refreshing the page on submit
     event.preventDefault();
 
@@ -13,14 +14,16 @@ const Input = ({ onSend }) => {
       return;
     }
 
-    onSend(message);
+    setFetching(true);
     setMessage('');
+    await onSend(message);
+    setFetching(false);
   };
 
   return (
     <Box component="form" onSubmit={handleSend} sx={{ display: 'flex', justifyContent: 'center', gap: '10px', p: 1 }}>
       <TextField value={message} onChange={e => setMessage(e.target.value)} label="Type your message..." variant="outlined" />
-      <Button type="submit" variant="contained">Send</Button>
+      <Button type="submit" variant="contained" disabled={fetching}>Send</Button>
     </Box>
   );
 };
