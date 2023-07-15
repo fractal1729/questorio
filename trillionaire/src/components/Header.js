f647bfbd657e7cb2d574d9c6687aa5375bb7ee51
Mihir Patel <mihir.v.patel7@gmail.com>
2023-07-15 16:20:29 -0700

    win con

--- trillionaire/src/components/Header.js
+++ trillionaire/src/components/Header.js
@@ -1,14 +1,16 @@
-import React from 'react';
+// Header.js
 import { Typography, Box } from '@mui/material';
 
-const Header = () => {
-  return (
-    <Box sx={{ width: '100%', p: 2, bgcolor: '#242525', color: '#fff' }}>
-      <Typography variant="h4" align="center">
-        Trillionaire!
-      </Typography>
-    </Box>
-  );
-};
+const Header = ({ title }) => (
+  <Box sx={{ height: '10%', width: '100%', p: 2, color: '#fff', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', bgcolor: '#242525' }}>
+    <Typography variant="h3" component="div" gutterBottom>
+      Questorio
+    </Typography>
+    <Typography variant="subtitle1" component="div" sx={{ fontStyle: 'italic' }}>
+      Complete the adventure in as few steps as possible (or jailbreak the LLM to reach the end condition...) to win!
+    </Typography>
+  </Box>
+);
 
 export default Header;
+
